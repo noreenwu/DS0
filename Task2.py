d78209c16e6323d185f66066c2d1f1f757b0984e
Noreen Wu <noreenwu@gmail.com>
2020-03-13 16:20:22 -0400

    clean: removed debug code and cleaned up output messages

--- Task2.py
+++ Task2.py
@@ -7,7 +7,7 @@ with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
 
-with open('calls-first-5.csv', 'r') as f:
+with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
@@ -31,31 +31,24 @@ def register_phone_call(ph, dur):
 
 
 def print_longest_message(phone_str, dur):
-    print (phone_str + " spent the longest time, ", dur, "seconds, on the phone during September 2016.")
+    print (phone_str + " spent the longest time, " + str(dur) + " seconds, on the phone during September 2016.")
 
 
 for i in range(len(calls)):
-    # print (calls[i][3])
     register_phone_call(calls[i][0], int(calls[i][3]))
     register_phone_call(calls[i][1], int(calls[i][3]))
 
 
-print("max:")
+
 max_key = max(phone_time, key=phone_time.get)
-print (max_key)
 max_val = phone_time[max_key]
 
-print ("max value is ", max_val)
-print(max(phone_time, key=phone_time.get))
-
-
-maxes = []
+maxes = []    # often the maxes occur in pairs, since both the caller and callee are counted
 for key, value in phone_time.items():
     if value == max_val:
         maxes.append(key)
 
-print("the maxes were ", maxes)
-
+# print the message: which number(s) spent the longest time
 if len(maxes) == 1:
     print_longest_message(str(maxes[0]), max_val)
 
