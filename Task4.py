6208e82f611104fc8f248eb27064a1214bc58335
Noreen Wu <noreenwu@gmail.com>
2020-03-21 14:41:15 -0400

    clean/fix/doc: removed unused, fixed comments and output

--- Task4.py
+++ Task4.py
@@ -28,37 +28,29 @@ The list of numbers should be print out one per line in lexicographic order with
 # create a set of unique numbers making phone calls
 uniq_callers = set()
 disqualified_nums = {}
-print ("num calls ", len(calls))
+
 for ph in calls:
-    # print(ph[0])
     uniq_callers.add(ph[0])
-    # print("adding receiver as disqualified num ", ph[1])
-    disqualified_nums[ph[1]] = 1
+    disqualified_nums[ph[1]] = 1   # any number receiving a call is disqualified
 
-print("num disqualified nums after looping thru calls ", len(disqualified_nums))
 
-# create a list/dict of numbers sending texts or receiving texts, from texts.csv
+# add to dict of disqualified any numbers sending texts or receiving texts, from texts.csv
 for txt in texts:
-    # print(txt[0])
-    # print (txt[1])
     disqualified_nums[txt[0]] = 1
     disqualified_nums[txt[1]] = 1
 
 
-# include in that list/dict numbers that have received calls (from 2nd col, calls.csv)
-print("disqualifying nums: ", len(disqualified_nums))
-
-print("uniq callers:", len(uniq_callers))
-
-# iterate through the set of unique numbers and delete anything found in the disqualifying dict
+# iterate through the set of unique numbers and do not include anything found in the disqualifying dict
 possible_telemarketer = []
 for u in uniq_callers:
     if u not in disqualified_nums:
-        # print("disqualifying one")
         possible_telemarketer.append(u)
 
 
-for t in possible_telemarketer:
+possible_telemarketer_sorted = sorted(possible_telemarketer)
+
+print ("These numbers could be telemarketers:")
+for t in possible_telemarketer_sorted:
     print(t)
 
 
